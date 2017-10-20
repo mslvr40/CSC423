4d4043d89912c473298669337760ccfa4d9b56e3
valek1 <32499264+valek1@users.noreply.github.com>
2017-10-20 19:47:48 -0400

    Basics finished.

--- WebApp/view/new_promotion_ui.php
+++ WebApp/view/new_promotion_ui.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>New Promotion</title>
+	<title>Add New Promotion</title>
 </head>
 <body>
 
@@ -9,19 +9,35 @@
 		<form action='../model/new_promotion.php' method='post'>
 			<table>
 				<tr>
+					<td>Promotion Name:</td>
+					<td><input type="text" name="promoName" id="promoName"</td>
+				</tr>
+				<tr>
+				
+				<tr>
+					<td>Promotion ID:</td>
+					<td><input type="text" name="promoID" id="promoID"></td>
+				</tr>
+				<tr>
 					<td>Amount off:</td>
 					<td><input type="text" name="promoAmount" id="promoAmount"></td>
 				</tr>
 				<tr>
 					<td>Type:</td>
 					<td><select name="typeOff" id="typeOff">
-						<option selected="selected">Select the discount type</option>
+						<option selected disabled>Select the discount type</option>
 						<option>%</option>
 						<option>$</option>
 						</select>
 					</td>
 				</tr>
+				<tr>
+					<td>Description:</td>
+					<td><input type="text" name="promoDescription" id="promoDescription"</td>
+				</tr>
 			</table>
+			<input type = "submit" value="Submit">
+			<input type = "button" onClick="window.location='index.html'" value="Back">
 		</form>
 	</center>
 </body>

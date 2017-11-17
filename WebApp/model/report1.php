6af44247d1b100ab07559470cad0e87b24eb8777
mslvr40 <mslvr40@aol.com>
2017-11-16 22:35:28 -0500

    Fixed Report 1. also ran an auto indent on your files

--- WebApp/model/report1.php
+++ WebApp/model/report1.php
@@ -1,49 +1,52 @@
 <html>
 <head></head>
-	<body>
-<?php
-require('../view/report1_result.inc');
-require('../db.inc');
+<body>
+	<?php
+	echo "<script type='text/javascript'>alert('1');</script>";
+	require('../view/report1_result.inc');
+	echo "<script type='text/javascript'>alert('2');</script>";
+	require('../db.inc');
+	echo "<script type='text/javascript'>alert('3');</script>";
 
-insert_promo();
+	insert_promo();
 
-function insert_promo()
-{
-	echo "<script>alert('djbfsjfb')</script>";
-	connect_and_select_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
-	$date = $_POST['date'];
-	
-	
-	$query = "SELECT AdEvent.Name, Promotion.Name FROM AdEvent, AdEventPromotion, Promotion WHERE AdEventPromotion.PromoCode = Promotion.PromoCode AND AdEventPromotion.EventCode = AdEvent.EventCode AND '$date' BETWEEN AdEvent.StartDate AND AdEvent.EndDate";
-	
-	$result = mysql_query($query);
-	$output = "";
-	
-	if (!$result){
-		$output = "Error in inserting Promotion $id: $nameMessage: ". mysql_error();
+	function insert_promo()
+	{
+		echo "<script type='text/javascript'>alert('4');</script>";
+		connect_and_select_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
+		$date = $_POST['date'];
+		
+		
+		$query = "SELECT AdEvent.Name, Promotion.Name FROM AdEvent, AdEventPromotion, Promotion WHERE AdEventPromotion.PromoCode = Promotion.PromoCode AND AdEventPromotion.EventCode = AdEvent.EventCode AND '$date' BETWEEN AdEvent.StartDate AND AdEvent.EndDate";
+		
+		$result = mysql_query($query);
+		$output = "";
+		
+		if (!$result){
+			$output = "Error in inserting Promotion $id: $nameMessage: ". mysql_error();
+		}
+		else{
+			$output = "Promotion $id: $nameMessage: inserted successfully.";
+		}
+		
+		show_report1_result($result, $output, $date);
+		
 	}
-	else{
-		$output = "Promotion $id: $nameMessage: inserted successfully.";
-	}
-	
-	show_report1_result($result, $output, $date);
-	
-}
 
-function connect_and_select_db($server, $username, $pwd, $dbname){
-	$conn = mysql_connect($server, $username, $pwd);
-	
-	if(!$conn){
-		echo "Unable to connect to DB: " . mysql_error();
-		exit;
+	function connect_and_select_db($server, $username, $pwd, $dbname){
+		$conn = mysql_connect($server, $username, $pwd);
+		
+		if(!$conn){
+			echo "Unable to connect to DB: " . mysql_error();
+			exit;
+		}
+		
+		$dbh = mysql_select_db($dbname);
+		if(!$dbh){
+			echo "Unable to select ".$dbname.": " . mysql_error();
+			exit;
+		}
 	}
-	
-	$dbh = mysql_select_db($dbname);
-	if(!$dbh){
-		echo "Unable to select ".$dbname.": " . mysql_error();
-		exit;
-	}
-}
-?>
+	?>
 </body>
 </html>

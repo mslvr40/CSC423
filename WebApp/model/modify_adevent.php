955cd942e114d14451a157350ce5ebe327b44e0c
valek1 <32499264+valek1@users.noreply.github.com>
2017-11-21 22:22:50 -0500

    Update modify_adevent.php

--- WebApp/model/modify_adevent.php
+++ WebApp/model/modify_adevent.php
@@ -16,8 +16,8 @@
 		$adEnd = mysql_real_escape_string($_POST['AdEnd']);
 		$adDesc = mysql_real_escape_string($_POST['AdDesc']);
 		$adEventType = mysql_real_escape_string($_POST['AdEventType']);
-		$query = "UPDATE AdEvent SET EventCode = '$adCode', Name = '$adName', StartDate = 'adStart', 
-		EndDate = '$adEnd', Description = '$adDesc', AdType = '$adEventType' WHERE EventCode = $number";
+		$query = "UPDATE AdEvent SET EventCode = '$adCode', Name = '$adName', StartDate = '$adStart', 
+		EndDate = '$adEnd', Description = '$adDesc', AdType = '$adEventType' WHERE AdCode = $number";
 
 		$result = mysql_query($query);
 		$message = "";
@@ -27,7 +27,7 @@
 		}
 		else
 		{
-			$message = "Ad Event $number: $desMessage: was modified successfully.";
+			$message = "Ad Event $desMessage: $number: was modified successfully.";
 			
 		}
 		

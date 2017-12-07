21491d78253a2c263376c701edafbfe0a522e520
valek1 <32499264+valek1@users.noreply.github.com>
2017-12-07 00:24:58 -0500

    Update report4.php

--- WebApp/model/report4.php
+++ WebApp/model/report4.php
@@ -8,7 +8,7 @@
 	function insert_promo()
 	{
 		connect_and_select_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
-		$query = "SELECT AdEvent.Name AS AdEvent_Name, Item.ItemNumber, Item.ItemDescription, Item.FullRetailPrice, PromotionItem.SalePrice, (Item.FullRetailPrice-PromotionItem.SalePrice) AS Savings
+	      $query = "SELECT AdEvent.Name AS AdEvent_Name, Item.ItemNumber, Item.ItemDescription, Item.FullRetailPrice, PromotionItem.SalePrice, (Item.FullRetailPrice-PromotionItem.SalePrice) AS Savings
               FROM Item
               INNER JOIN PromotionItem ON Item.ItemNumber = PromotionItem.ItemNumber
               INNER JOIN Promotion ON PromotionItem.PromoCode = Promotion.PromoCode
@@ -16,13 +16,14 @@
               INNER JOIN AdEvent ON AdEventPromotion.EventCode = AdEvent.EventCode
               ORDER BY (Item.FullRetailPrice - PromotionItem.SalePrice) DESC
               LIMIT 50";
+		#$query = "SELECT * FROM Item LIMIT 50;
 		$result = mysql_query($query);
 		$output = "";
 		if (!$result){
 			$output = "Error in finding promo with these variables: $off, $type : ". mysql_error();
 		}
 		
-		show_report4_result();
+		show_report4_result($result);
 	}
 	function connect_and_select_db($server, $username, $pwd, $dbname){
 		$conn = mysql_connect($server, $username, $pwd);
